e7f32aa980b1a97c2f94e74c4256264715746628
ryosuketter <r.shimizu.yale@gmail.com>
2023-06-20 00:07:08 +0900

    Added NavigationContainer to App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>MMM</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
